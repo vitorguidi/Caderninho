253572873b0afc8fdbfabaabcab8ca080782dbc3
guidi <vitorguidi@gmail.com>
2018-09-14 13:22:47 -0300

    refatorada lazyseg, adicionado unlazy

--- data structures/lazyseg.cpp
+++ data structures/lazyseg.cpp
@@ -1,17 +1,31 @@
 #include "bits/stdc++.h"
 using namespace std;
 
+int seg[400400];
+int lazy[400400];
+
 void build(int node, int l, int r){		//constroi igual a seg normal
 
 	if(l==r){
-		segtree[node]=v[i];
+		seg[node]=v[l];
 		return;
 	}
 
 	int mid = (l+r)/2;
 	build(2*node,l,mid);
 	build(2*node+1,mid+1,r);
-	segtree[node]=segtree[2*node]+segtree[2*node+1];
+	seg[node]=seg[2*node]+seg[2*node+1];
+
+}
+
+void unlazy(int node, int l, int r){
+
+	seg[node]+=(r-l+1)*lazy[node];
+	if(l!=r){
+		lazy[2*node]+=lazy[node];
+		lazy[2*node+1]+=lazy[node];
+	}
+	lazy[node]=0;
 
 }
 
@@ -19,19 +33,12 @@ void update(int node, int l, int r, int a, int b, int val){
 
 	int lc=2*node;	int rc=2*node+1;	int mid = (l+r)/2;
 
-	if(lazy[node]){		//lida com atualizacao pendente. so preciso aplicar no inicio de uma consulta ou update
-		segtree[node]+=lazy[node]*(r-l+1);
-		if(l!=r){
-			lazy[lc]+=lazy[node];
-			lazy[rc]+=lazy[node];
-		}
-		lazy[node]=0;
-	}
+	unlazy(node,l,r);
 
 	if(r<a || l>b || l>r)	return;		//intervalo invalido
 
 	else if(a<=l && r<=b){		//update cobre o no
-		segtree[node]+=val*(r-l+1);
+		seg[node]+=val*(r-l+1);
 		if(r!=l){
 			lazy[lc]+=val;
 			lazy[rc]+=val;
@@ -40,8 +47,8 @@ void update(int node, int l, int r, int a, int b, int val){
 
 	else{		//no parcialmente contido
 		update(2*node,l,mid,a,b,val);
-		update(2*node+1,mid+1,a,b,val);
-		segtree[node]=segtree[lc]+segtree[rc];
+		update(2*node+1,mid+1,r,a,b,val);
+		seg[node]=seg[lc]+seg[rc];
 	}
 
 }
@@ -50,18 +57,11 @@ int query(int node, int l, int r, int a, int b){
 
 	int lc=2*node; int rc=2*node+1; int mid = (l+r)/2;
 
-	if(lazy[node]){
-		segtree[node]+=lazy[node]*(r-l+1);
-		if(l!=r){
-			lazy[lc]+=lazy[node];
-			lazy[rc]+=lazy[node];
-		}
-		lazy[node]=0;
-	}	
+	unlazy(node,l,r);
 
 	if(r<a || l>b || l>r)	return	0;	//ou qualquer indicador invalido, depende
 
-	else if(a<=l && r<=b)	return segree[node];
+	else if(a<=l && r<=b)	return seg[node];
 
 	else	return query(lc,l,mid,a,b) + query(rc,mid+1,r,a,b);
 
