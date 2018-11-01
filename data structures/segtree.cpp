8af9fc28b2d8c5751650669a42a9f33f2bfb58ce
guidi <vitorguidi@gmail.com>
2018-11-01 14:29:54 -0200

    atualizada seg para o bixecamp poli

--- data structures/segtree.cpp
+++ data structures/segtree.cpp
@@ -1,55 +1,50 @@
 #include "bits/stdc++.h"
 using namespace std;
 
+int seg[4*MAXN],v[MAXN];	//arvore soh tem 2*n pra n=power of 2
 
-int segtree[4*MAXN],v[MAXN];	//arvore soh tem 2*n pra n=power of 2
-
-void build(int node, int ini, int end){
+void build(int node, int l, int r){
 	
-	if(ini==end)	segtree[node]=v[ini];
-	
-	else{
-		int mid	= (ini+end)/2;
-		
-		build(2*node,ini,mid);		//constroi os filhos
-		build(2*node+1,mid+1,end);
-
-		segtree[node]=segtree[2*node] * segtree[2*node+1];	//compoe os filhos pra atualizar no, estou interessado no produto
+	if(l==r){
+		seg[node]=v[l];
+		return;
 	}
+	
+	int mid	= (l+r)/2;
+	
+	build(2*node,l,mid);		//constroi os filhos
+	build(2*node+1,mid+1,r);
+
+	seg[node] = seg[2*node] + seg[2*node+1];	//compoe os filhos pra atualizar no, estou interessado no produto
 
 }
 
-void update(int node, int ini, int end, int idx, int val){
+void update(int node, int l, int r, int id, int val){
 
-	if(ini==end){					//idx= id do elemento que eu qro trocar em v, val eh o novo valor (1,0 ou -1)
-		v[idx]=val;					//se chego ao conjunto de 1 elemento, simplesmente atualizo valor
-		segtree[node]=val;
+	if(l==r){					//id= id do elemento que eu qro trocar em v, val eh o novo valor (1,0 ou -1)
+		v[l]=val;					//se chego ao conjunto de 1 elemento, simplesmente atualizo valor
+		return;
 	}
 
-	else{
-		int mid = (ini+end)/2;
-		
-		if(ini<=idx && idx<=mid)	update(2*node,ini,mid,idx,val);			//se o cara estiver no filho da esquerda, atualizo filho esquerda
-		else						update(2*node+1,mid+1,end,idx,val);		//caso contrario, vou pra direita
+	int mid = (l+r)/2;
+	
+	if(l<=id && id<=mid)	update(2*node,l,mid,id,val);			//se o cara estiver no filho da esquerda, atualizo filho esquerda
+	else					update(2*node+1,mid+1,r,id,val);		//caso contrario, vou pra direita
 
-		segtree[node]=segtree[2*node] * segtree[2*node+1];	//estou interessado em produtos nos intervalos
-
-	}
+	seg[node]=seg[2*node] + seg[2*node+1];	//estou interessado em produtos nos intervalos
 
 }
 
 
-int query(int node, int ini, int end, int a, int b){	//procura o intervalo [a,b], e o noh eh o intervalo [ini,end]
+int query(int node, int l, int r, int a, int b){	//procura o intervalo [a,b], e o noh eh o intervalo [l,r]
 
-	if (a>end || b<ini)		return 1;		//range invalida, [ini,end] tem elementos fora de [a,b], q eh o intervalo original
+	if (a>r || b<l)		return 0;		//range invalida, [l,r] tem elementos fora de [a,b], q eh o intervalo original
 
-	if(a<=ini && end<=b)	return segtree[node];	//range contida no intervalo de interesse, retorna resultado
+	if(a<=l && r<=b)	return seg[node];	//range contida no intervalo de interesse, retorna resultado
 
-	int mid=(ini+end)/2;
-	int p1 = query(2*node,ini,mid,a,b);				//se estiver parcialmente contido, continuo buscando e concateno os resultados
-	int p2 = query(2*node+1,mid+1,end,a,b);
-
-	return p1*p2;
+	int mid=(l+r)/2;
+	
+	return query(2*node,l,mid,a,b) + query(2*node+1,mid+1,r,a,b);
 
 }
 
