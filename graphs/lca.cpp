1e731b2ee4c74c688f9c9728128e7322db9d0eb4
guidi <vitorguidi@gmail.com>
2018-11-23 07:43:34 -0200

    melhorado LCa no caderno, tirei fastxp de matriz pq tava zuado. um dia arrumo

--- graphs/lca.cpp
+++ graphs/lca.cpp
@@ -7,42 +7,40 @@ using namespace std;
 vector<int> nodes[MAXN];
 
 int memo[MAXN][LMAXN];
-int pai[MAXN];
 int hgt[MAXN];
 int vis[MAXN]
 int n;
 
 void dfs(int v){
+
 	vis[v]=1;
+	
 	for(auto filho : nodes[v]){
+	
 		if(vis[filho])	continue;
-		pai[filho]=v;
+	
+		memo[filho][0]=v;		//seta o pai do cara
 		hgt[filho]=hgt[v]+1;
+		
+		for(int i=1;i<LMAX;i++)	
+			memo[filho][i]=memo[memo[filho][i-1]][i-1];
+								//gera a sparse table na propria dfs (dependencias ja estao calculadas, por inducao)
 		dfs(filho);
 	}
-}
-
-void build_table(){
-
-	dfs(0);	//seta alturas a partir da raiz
-	
-	for(int i=0;i<n;i++)	memo[i][0]=pai[i];	//inicializa pai de ordem 2^0 de todos como o pai deles
-
-	for(int i=1; i<=LMAXN;i++)											//calcula a sparse table pras potencias de 2 apropriadas
-		for(int j=0;j<n;j++)	memo[j][i]=memo[ memo[j][i-1] ][i-1];	//calcula o role pra todos os vertices		
 
 }
 
 int lca(int a, int b){
+
 	if(hgt[a]<hgt[b])	swap(a,b);		//quero que a seja o cara mais profundo na arvore
 
-	for(int i=LMAXN; i>=0; i-- ){						//bota a e b na mesma altura
+	for(int i=LMAXN-1; i>=0; i-- ){						//bota a e b na mesma altura
 		if(hgt[ memo[a][i] ] >= hgt[b])	a=memo[a][i];	//vou subir o a ate ele ficar no mesmo nivel do b
 	}
 
 	if(a==b)	return a;	//se forem iguais, retorna o LCA
 
-	for(int i=LMAXN; i>=0; i--){	//vai pulando ate ficarem ambos a e b um nivel antes do LCA
+	for(int i=LMAXN-1; i>=0; i--){	//vai pulando ate ficarem ambos a e b um nivel antes do LCA
 		
 		if(memo[a][i]!=-1 && memo[a][i]!=memo[b][i]){
 			a=memo[a][i];
@@ -51,7 +49,7 @@ int lca(int a, int b){
 
 	}
 
-	return memo[b][0];
+	return memo[b][0];	//sobe um nivel e retorna o lca
 
 }
 
